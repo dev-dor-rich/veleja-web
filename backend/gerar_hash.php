168f270d0e8163cab387095a14bade1d0a3937b0
Richard <veleja.nav@gmail.com>
2026-08-22 15:23:11 -0300

    Move Dockerfile para backend/ para deploy no Railway

--- backend/gerar_hash.php
+++ backend/gerar_hash.php
@@ -1,9 +1,15 @@
 <?php
-// Gere um hash seguro para cada senha
-$senha = '8484Richard#'; 
-$hash = password_hash($senha, PASSWORD_BCRYPT);
-echo "Hash gerado: " . $hash;
+$senhas = [
+    'yasmim@veleja.com' => 'gatinhos123',
+    'cofrinhochurros@veleja.com' => 'Raisethevil67',
+];
 
-// O link abaixo agora é um comentário (o PHP vai ignorar ele)
-// http://localhost:8000/gerar_hash.php
+foreach ($senhas as $email => $senha) {
+    $hash = password_hash($senha, PASSWORD_BCRYPT);
+    echo "<pre>";
+    echo "Email: $email\n";
+    echo "Hash: $hash\n";
+    echo "UPDATE admins SET senha_hash = '$hash' WHERE email = '$email';\n";
+    echo "</pre>";
+}
 ?>
